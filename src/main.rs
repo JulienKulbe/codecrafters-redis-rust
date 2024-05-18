c6a55e718b0441ad06f281e092aac3225b39ab16
Julien Kulbe <julien.kulbe@googlemail.com>
2024-05-18 18:09:01 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use anyhow::{bail, Result};
 use std::{
-    io::{BufRead, BufReader, BufWriter, Write},
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
     thread,
-    time::Duration,
 };
 
 fn main() {
@@ -28,45 +27,25 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) -> Result<()> {
-    let mut writer = BufWriter::new(stream.try_clone()?);
-    let mut reader = BufReader::new(&mut stream);
-
     loop {
-        let mut line = String::new();
-        let received = reader.read_line(&mut line)?;
-        if received == 0 {
-            //return Ok(());
-            thread::sleep(Duration::from_millis(100));
-            continue;
-        }
+        let mut buffer = [0_u8; 512];
 
-        let mut request = Vec::new();
-        line = line.trim_end().to_string();
-        if let Some(line_count) = line.strip_prefix('*') {
-            let line_count = line_count.parse::<usize>()?;
-            for _ in 0..2 * line_count {
-                let mut line = String::new();
-                reader.read_line(&mut line)?;
-                line = line.trim_end().to_string();
-                request.push(line);
-            }
-        }
+        let bytes = stream.read(&mut buffer)?;
+        let request = String::from_utf8_lossy(&buffer);
+        println!(">> {request} ({bytes})");
 
-        if request.is_empty() || request.len() % 2 != 0 || request.len() < 2 {
-            bail!(
-                "Invalid line count, expected number of lines: {}, received {}",
-                line,
-                request.len()
-            );
-        }
+        let request = request.split("\r\n").collect::<Vec<&str>>();
 
-        let command = request[1].to_uppercase();
+        // TODO verify request len
+
+        let command = request[2].to_uppercase();
         let response = match command.as_str() {
             "PING" => "+PONG\r\n".to_string(),
             "ECHO" => format!("$3\r\n{}\r\n", request[3]),
+            "CLIENT" => "+OK\r\n".to_string(),
             _ => bail!("unsupportd command: {}", command),
         };
 
-        writer.write_all(response.as_bytes())?;
+        stream.write_all(response.as_bytes())?;
     }
 }
