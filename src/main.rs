9934cf944a381d8e197100a2e32306a2343c8cd9
Julien Kulbe <julien.kulbe@googlemail.com>
2024-05-18 15:12:41 +0200

    send pong

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::net::TcpListener;
+use anyhow::Result;
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,12 +12,26 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                _ = handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) -> Result<()> {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_lines = buf_reader.lines();
+    for request_line in request_lines.map_while(Result::ok) {
+        println!("{request_line}");
+    }
+
+    let response = "+PONG\r\n";
+    stream.write_all(response.as_bytes())?;
+
+    Ok(())
 }
