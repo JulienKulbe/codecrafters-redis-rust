666c0a31b1fc1a1d730cb27a3856a3a310a98ebf
Julien Kulbe <julien.kulbe@googlemail.com>
2024-05-18 17:42:15 +0200

    implemented echo command, cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use std::{
-    io::{BufRead, BufReader, Read, Write},
+    io::{BufRead, BufReader, BufWriter, Write},
     net::{TcpListener, TcpStream},
     thread,
-    time::Duration,
 };
 
 fn main() {
@@ -28,33 +27,43 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) -> Result<()> {
-    // let mut buffer = [0_u8; 12];
-
-    // let bytes = stream.read(&mut buffer)?;
-    // let request = String::from_utf8_lossy(&buffer);
-    // println!(">> {request} ({bytes})");
+    let mut writer = BufWriter::new(stream.try_clone()?);
+    let mut reader = BufReader::new(&mut stream);
 
     loop {
-        let mut reader = BufReader::new(&mut stream);
-
-        let received: Vec<u8> = reader.fill_buf()?.to_vec();
-        if received.is_empty() {
-            thread::sleep(Duration::from_millis(100));
-            continue;
+        let mut line = String::new();
+        let received = reader.read_line(&mut line)?;
+        if received == 0 {
+            return Ok(());
         }
 
-        let request = String::from_utf8_lossy(&received);
-        reader.consume(received.len());
-        println!(">> {request} ({})", received.len());
-
-        let lines: Vec<&str> = request.split_terminator("\r\n").collect();
-        let commands = lines.len() / 3;
-
-        for _ in 0..commands {
-            let response = "+PONG\r\n";
-            println!("<< {response}");
-
-            stream.write_all(response.as_bytes())?;
+        let mut request = Vec::new();
+        line = line.trim_end().to_string();
+        if let Some(line_count) = line.strip_prefix('*') {
+            let line_count = line_count.parse::<usize>()?;
+            for _ in 0..2 * line_count {
+                let mut line = String::new();
+                reader.read_line(&mut line)?;
+                line = line.trim_end().to_string();
+                request.push(line);
+            }
         }
+
+        if request.is_empty() || request.len() % 2 != 0 || request.len() < 2 {
+            bail!(
+                "Invalid line count, expected number of lines: {}, received {}",
+                line,
+                request.len()
+            );
+        }
+
+        let command = request[1].to_uppercase();
+        let response = match command.as_str() {
+            "PING" => "+PONG\r\n".to_string(),
+            "ECHO" => format!("$3\r\n{}\r\n", request[3]),
+            _ => bail!("unsupportd command: {}", command),
+        };
+
+        writer.write_all(response.as_bytes())?;
     }
 }
